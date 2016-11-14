6f5546bc4c7b2928378d4fb10801cb93e34146af
Jan Schulte <j.schulte@52north.org>
2016-11-14 10:43:09 +0100

    support DbQueryFactory

---
+++ proxy/src/main/java/org/n52/series/db/dao/ProxyDbQueryFactory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.series.db.dao;
+
+import org.n52.io.request.IoParameters;
+
+/**
+ *
+ * @author jansch
+ */
+public class ProxyDbQueryFactory implements DbQueryFactory {
+
+    @Override
+    public DbQuery createFrom(IoParameters parameters) {
+        return ProxyDbQuery.createFrom(parameters);
+    }
+
+}
