158ad197ffde7f459e7ad0abc3077cb85e5e1cbd
antego <antego@users.noreply.github.com>
2017-03-12 15:10:06 +0300

    Dump response body, hardcode series-dao version with compatible bindings

--- proxy-cache/src/main/java/org/n52/proxy/cache/CachingFilter.java
+++ proxy-cache/src/main/java/org/n52/proxy/cache/CachingFilter.java
@@ -8,10 +8,15 @@ import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Set;
 
 public class CachingFilter implements Filter {
@@ -24,30 +29,20 @@ public class CachingFilter implements Filter {
         LOG.info("Caching filter initialized");
     }
 
-    /**
+    /*
      * 1. Check if httprequest
      * 2. Check that it's a data request
      * 3. Check timespan param
      * 4. Go to the cache for that values
      * 5. Return values or continue request processing
      * 6. Write returned values to the cache
-     * @param servletRequest
-     * @param servletResponse
-     * @param filterChain
-     * @throws IOException
-     * @throws ServletException
      */
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         LOG.info("Doing caching filtering");
 
-        if (!(servletRequest instanceof HttpServletRequest)) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        if (!request.getPathInfo().endsWith("/data")) {
+        if (request.getPathInfo() == null || !request.getPathInfo().endsWith("/data")) {
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
@@ -58,12 +53,38 @@ public class CachingFilter implements Filter {
             return;
         }
 
-        filterChain.doFilter(servletRequest, servletResponse);
+        ResponseRecordingWrapper wrapper = new ResponseRecordingWrapper((HttpServletResponse) servletResponse);
+        filterChain.doFilter(request, wrapper);
+        LOG.info("Response payload:\n{}", wrapper.getBody());
         LOG.info("End of caching filtering");
     }
 
     @Override
     public void destroy() {
         LOG.info("Destroying caching filter");
+    }
+
+    class ResponseRecordingWrapper extends HttpServletResponseWrapper {
+        private ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        public ResponseRecordingWrapper(HttpServletResponse response) {
+            super(response);
+        }
+
+        @Override
+        public ServletOutputStream getOutputStream() throws IOException {
+            ServletOutputStream original = super.getOutputStream();
+            return new ServletOutputStream() {
+                @Override
+                public void write(int b) throws IOException {
+                    original.write(b);
+                    output.write(b);
+                }
+            };
+        }
+
+        public String getBody() {
+            return new String(output.toByteArray(), StandardCharsets.UTF_8);
+        }
     }
 }
--- proxy-cache/src/main/java/org/n52/proxy/cache/SimpleCache.java
+++ proxy-cache/src/main/java/org/n52/proxy/cache/SimpleCache.java
@@ -1,6 +1,9 @@
 package org.n52.proxy.cache;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SimpleCache {
